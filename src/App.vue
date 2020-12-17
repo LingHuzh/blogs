7db3ee8dcffa40a072dc495bbcced907c4d9fab3
linghuzhanghao <linghuzhanghao@163.com>
2020-12-17 14:18:21 +0800

    项目搭建提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div id="nav"></div>
     <router-view/>
   </div>
 </template>
@@ -13,20 +10,15 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body,html,a,p,b,span,h1,h2,h3,ul,ol,li,em,div{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+body{
+	background: #f6f8fa;
 }
 </style>
---
+++ src/components/index/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="lh_nav">
+        <div class="lh_nav_left">
+			<img src="../../images/heard.jpg" alt="头像">
+			<p>{{name}}</p>
+		</div>
+        <div class="lh_nav_right"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name:'',
+       components:{
+
+       },
+       data() {
+           return {
+               name:"LingHuZhangHao"
+           }
+       },
+    }
+</script>
+
+<style lang="scss" scoped>
+.lh_nav{
+    width: 100%;
+    height: 80px;
+    background: #24292e;
+    color: #fff;
+    font-size: 28px;
+}
+.lh_nav_left{
+	width: 30%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    img{
+        display: block;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        margin: 0 50px 0 10%;
+    }
+}
+.lh_nav_right{
+	width: 70%;
+}
+</style>
